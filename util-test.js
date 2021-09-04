db86b645ea8efc3dcbd1d9ceadc08ecb07103eee
andrew scoppa <ascoppa@live.com>
2021-09-04 16:47:52 -0400

    first commit

---
+++ util-test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const func_module = require('./util')
+
+function testSum() {
+  var x = 5;
+  var y = 1;
+  var sum1 = x + y;
+  var sum2 = func_module.add_numbers([5, 6]);
+
+  console.log('addTwoNumbers() should return the sum of its two parameters.');
+  console.log('Expect ' + sum1 + ' to equal ' + sum2 + '.');
+  
+  try {
+
+    assert.equal(sum1, sum2);
+    
+    console.log('Passed.');
+  } catch (error) {
+    console.error('Failed.');
+  }
+}
+
+testSum();
---
+++ util.js
@@ -0,0 +1,10 @@
+﻿function sum_array(ar) {
+    var total = 0;
+    ar.forEach(element => {
+        total += element;
+    });
+    return total;
+}
+module.exports = {
+    add_numbers : sum_array
+}
